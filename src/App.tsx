1c958f921c918f349f3880103b86ecbfeb5158bc
Francis Adewale <francisadewale1992@gmail.com>
2022-04-21 19:04:57 +0100

    can get song file from icon click

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 
+interface IState {
+  songUrl : ""
+}
+
+const [song, setSong] = useState<IState>()
+
+
 
 function App() {
+
+
+
   return (
     <div className='app-container'>
       <Header />
-      <MainContent />
+      <MainContent song={song} setSong={setSong} />
     </div>
   )
 }
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import sampleImg from "../imgs/sample.png";
 
-const MainContent = () => {
+interface IProps {
+    song : ""
+}
+
+const MainContent : React.FC<IProps> = ({song}) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleCapture = (event : React.ChangeEvent<HTMLInputElement>) : void => {
+
+        if (event.target.files && event.target.files[0]) {
+                let reader = new FileReader();
+                reader.onload = (e) => {
+                    console.log(e.target?.result)
+                    
+                };
+                reader.readAsDataURL(event.target.files[0]);
+              }
+        
+
+
+
+    }
+    const handleClick = () : void => {
+        if (inputRef && inputRef.current) {
+            console.log(inputRef.current.click());
+        }
+    }
+
     return (
         <main>
-            <img src={sampleImg} className="sample-img"/>
+            <img src={sampleImg} className="sample-img" onClick={handleClick}/>
+            <input type="file" ref={inputRef} style={{display: "none"}} onChange={(e) => handleCapture(e)}/>
             <h1>Upload song to sample</h1>
         </main>
     );
