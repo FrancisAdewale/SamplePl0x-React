a957de14d76c03047b5a597e13b838a6ca8d4a1b
Francis Adewale <francisadewale1992@gmail.com>
2022-04-25 14:38:33 +0100

    can play audio from selected file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import useSound from 'use-sound';
 
-interface IState {
-  songUrl : ""
+
+
+
+
+const App: React.FC = () => {
+
+  var audioCtx = new (window.AudioContext)();
+  var source = audioCtx.createBufferSource();
+  var dest = audioCtx.createMediaStreamDestination();
+  var mediaRecorder = new MediaRecorder(dest.stream);
+
+  
+  
+
+
+  const [song, setSong] = useState<string | ArrayBuffer | null | undefined>(null)
+
+  const updateSong = (newSong: string | ArrayBuffer | null | undefined):void => {
+    setSong(newSong)
+  
+
+
+    // audioCtx.decodeAudioData(song as ArrayBuffer, buffer => {
+    //   source.buffer = buffer;
+    //   source.connect(dest);
+    //   mediaRecorder.start();
+    //   source.start(audioCtx.currentTime, 3);
+    // }, e => {
+    //   console.log(`error with decoding: ${e}`)
+    // })
 }
 
-const [song, setSong] = useState<IState>()
+useEffect(() => {
+   var request = new XMLHttpRequest();
 
+    request.open('GET', song as string, true);
 
+    request.responseType = 'arraybuffer';
 
-function App() {
+    request.onload = function() {
+      var audioData = request.response;
+  
+      audioCtx.decodeAudioData(audioData, function(buffer) {
+          source.buffer = buffer;
+  
+          source.connect(audioCtx.destination);
+          source.loop = true;
+          source.start()
+        },
+  
+        function(e){ console.log("Error with decoding audio data" + e); });
+  
+    }
+
+    request.send();
+  console.log(song)
+}, [song]);
 
 
 
   return (
     <div className='app-container'>
       <Header />
-      <MainContent song={song} setSong={setSong} />
+      <MainContent updateSong={updateSong}/>
     </div>
   )
 }
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,39 +1,47 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import sampleImg from "../imgs/sample.png";
 
 interface IProps {
-    song : ""
+    updateSong : (arg: string | ArrayBuffer | null | undefined) => void
 }
 
-const MainContent : React.FC<IProps> = ({song}) => {
+const MainContent : React.FC<IProps> = ({updateSong}) => {
 
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const [chosenSongData, setChosenSongData] = useState<string | ArrayBuffer | null | undefined>("")
 
     const handleCapture = (event : React.ChangeEvent<HTMLInputElement>) : void => {
 
         if (event.target.files && event.target.files[0]) {
                 let reader = new FileReader();
                 reader.onload = (e) => {
-                    console.log(e.target?.result)
-                    
+  
+                const data = e.target!.result;
+                setChosenSongData(data);
+                updateSong(data)
+
                 };
+                
                 reader.readAsDataURL(event.target.files[0]);
               }
-        
-
 
 
     }
+
+    
     const handleClick = () : void => {
         if (inputRef && inputRef.current) {
-            console.log(inputRef.current.click());
+
+            inputRef.current.click();
+            
         }
     }
 
     return (
         <main>
             <img src={sampleImg} className="sample-img" onClick={handleClick}/>
-            <input type="file" ref={inputRef} style={{display: "none"}} onChange={(e) => handleCapture(e)}/>
+            <input type="file" ref={inputRef} style={{display: "none"}} onChange={handleCapture}/>
             <h1>Upload song to sample</h1>
         </main>
     );
