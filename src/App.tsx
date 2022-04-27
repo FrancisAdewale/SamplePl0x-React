35970115f93400c2a8f14c89dc7c7811a6bcca5d
Francis Adewale <francisadewale1992@gmail.com>
2022-04-27 12:47:00 +0100

    refactor app component

--- src/App.tsx
+++ src/App.tsx
@@ -6,73 +6,66 @@ import MainContent from './components/MainContent';
 import WaveSurfer from "wavesurfer.js";
 import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
 import RegionsPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
+import { on } from 'stream';
 
 
 
 
 const App: React.FC = () => {
 
-  const [uploadSong, setUploadedSong] = useState(false)
-
-  var audioCtx = new (window.AudioContext)();
-  var source = audioCtx.createBufferSource();
-  var dest = audioCtx.createMediaStreamDestination();
-  var mediaRecorder = new MediaRecorder(dest.stream);
-
-  const [wavesurferObj, setWaveSurferObj] = useState<WaveSurfer | null>();
-
-
-  const [song, setSong] = useState<string | ArrayBuffer | null | undefined>(null)
-  const [songUploaded, setSongUploaded] = useState<boolean>(false)
+  const [uploadSong, setUploadedSong] = useState(false);
+  const waveformRef = useRef(null);
+  const [wavesurferObj, setWaveSurferObj] = useState<WaveSurfer>();
+  const [song, setSong] = useState<string | ArrayBuffer | null | undefined>(null);
+  const [songUploaded, setSongUploaded] = useState<boolean>(false);
+  const [duration, setDuration] = useState(0); 
 
   const updateSong = (newSong: string | ArrayBuffer | null | undefined):void => {
     setSong(newSong)
     setUploadedSong(true)
 
-
 }
 
 useEffect(() => {
-          setWaveSurferObj(WaveSurfer.create({
-            container: '#waveform',
-            waveColor: '#14213D',
-            progressColor: '#FCA311',
-            responsive: true,
-            plugins : [
-            RegionsPlugin.create({
-              dragSelection: {
-                slop: 3,
-                
-            }
-              
-            })
 
-            ]
-           
-         
-        }));
+  if(waveformRef.current) {
 
-        wavesurferObj?.regions.enableDragSelection({})
+    setWaveSurferObj(WaveSurfer.create({
+      container: waveformRef.current,
+      waveColor: '#14213D',
+      progressColor: '#FCA311',
+      responsive: true,
+      plugins : [
+      RegionsPlugin.create({
+        dragSelection: {
+          slop: 3,
+        }
+      })
+      ]
+    }))
+  }
+}, [song]);
 
-      
-        
-        wavesurferObj?.init()
-        
-        wavesurferObj?.load(song as string);
+useEffect(() => {
+  wavesurferObj?.init();
+  wavesurferObj?.load(song as string);
+  wavesurferObj?.regions.enableDragSelection({})
+  wavesurferObj?.on('ready', () => {      
+  });
 
-        wavesurferObj?.on('ready', () => {
-         
-      });
-
-      wavesurferObj?.on('region-dblclick', () => {
-
-        wavesurferObj?.regions.clear()
-
-      });
-  
+  wavesurferObj?.on('region-dblclick', region => {
+   region.remove()
    
+});
 
-}, []);
+wavesurferObj?.on('region-created', region => {
+  const regions = region.wavesurfer.regions.list;
+  console.log(regions);
+
+
+})
+
+}, [wavesurferObj]);
 
 
 
@@ -83,27 +76,15 @@ useEffect(() => {
 
       {
       uploadSong
-
       ?
-
-        <div id='waveform' style={{
+        <div ref={waveformRef} style={{
           width: "150vh",
           height: "100vh",
           margin: "200px auto 0 auto"
-        }}></div>
-      
+        }} ></div>
       :
-
       <MainContent updateSong={updateSong}/> 
-
-      
-
       }
-
-      <div id='wave-timeline' style={{
-          width: "150vh",
-          margin: "0 auto"
-        }}></div>
     </div>
   )
 }
